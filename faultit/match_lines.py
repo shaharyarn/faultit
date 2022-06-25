91960940816857ad4860fd3fb2295409104bfef4
shahar.yarn <shahar.yarn@gmail.com>
2022-06-25 12:45:36 +0300

    Refactor line matching

--- faultit/match_lines.py
+++ faultit/match_lines.py
@@ -29,13 +29,31 @@ def _get_similarity_score(first: bytes, second: bytes) -> int:
 def match_lines(
     old_lines: List[bytes], new_lines: List[bytes], all_removed_lines: Dict[int, bytes]
 ) -> Tuple[Dict[Tuple[int, ...], Tuple[int, ...]], List[Tuple[int, bytes, int, bytes]]]:
+    matching, from_line_index, external_matching = _generate_matching(
+        old_lines, new_lines, all_removed_lines
+    )
+
+    line_matching: Dict[Tuple[int, ...], Tuple[int, ...]] = {}
+    for i in range(from_line_index):
+        line_matching[(i,)] = tuple(
+            (key for key, value in matching.items() if value == i)
+        )
+
+    line_matching[tuple(range(from_line_index, len(old_lines)))] = tuple(
+        (key for key, value in matching.items() if value == from_line_index)
+    )
+    return line_matching, external_matching
+
+
+def _generate_matching(
+    old_lines: List[bytes], new_lines: List[bytes], all_removed_lines: Dict[int, bytes]
+) -> Tuple[Dict[int, int], int, List[Tuple[int, bytes, int, bytes]]]:
     matching: Dict[int, int] = {}
     external_matching: List[Tuple[int, bytes, int, bytes]] = []
     from_line_index = 0
 
-    # match new line to old line
     for index, new_line in enumerate(new_lines):
-        match = find_match_in_dict(
+        match = _find_match(
             {
                 old_line_index: old_line
                 for old_line_index, old_line in list(enumerate(old_lines))[
@@ -54,48 +72,37 @@ def match_lines(
                 (match[0], all_removed_lines[match[0]], index, new_line)
             )
 
-    line_matching: Dict[Tuple[int, ...], Tuple[int, ...]] = {}
-    for i in range(from_line_index):
-        line_matching[(i,)] = tuple(
-            (key for key, value in matching.items() if value == i)
-        )
+    return matching, from_line_index, external_matching
 
-    line_matching[tuple(range(from_line_index, len(old_lines)))] = tuple(
-        (key for key, value in matching.items() if value == from_line_index)
+
+def _get_best_match_index(
+    line: bytes,
+    line_index: int,
+    index_to_line: Dict[int, bytes],
+) -> Tuple[int, int]:
+    return max(
+        [
+            (index, _get_similarity_score(indexed_line, line))
+            for index, indexed_line in index_to_line.items()
+        ],
+        key=lambda x: (x[1], -abs(x[0] - line_index)),
     )
-    return line_matching, external_matching
 
 
-def find_match_in_dict(
+def _find_match(
     index_to_line: Dict[int, bytes],
     line: bytes,
     line_index: int,
     other_lines: Dict[int, bytes],
 ) -> Tuple[int, bool]:
+    best_match_in_removed = _get_best_match_index(line, line_index, other_lines)
 
-    if index_to_line:
-        best_line = max(
-            [
-                (index, _get_similarity_score(indexed_line, line), True)
-                for index, indexed_line in index_to_line.items()
-            ],
-            key=lambda x: (x[1], -abs(x[0] - line_index)),
-        )
-        if best_line[0] <= 0:
-            best_line = max(
-                [best_line]
-                + [
-                    (index, _get_similarity_score(indexed_line, line) - 5, False)
-                    for index, indexed_line in other_lines.items()
-                ],
-                key=lambda x: (x[1], -abs(x[0] - line_index)),
-            )
-    else:
-        best_line = max(
-            [
-                (index, _get_similarity_score(indexed_line, line) - 5, False)
-                for index, indexed_line in other_lines.items()
-            ],
-            key=lambda x: (x[1], -abs(x[0] - line_index)),
-        )
-    return best_line[0], best_line[2]
+    if not index_to_line:
+        return best_match_in_removed[0], False
+
+    best_match_in_curr = _get_best_match_index(line, line_index, index_to_line)
+
+    if best_match_in_curr[1] <= best_match_in_removed[1] - 5:
+        return best_match_in_removed[0], False
+
+    return best_match_in_curr[0], True
